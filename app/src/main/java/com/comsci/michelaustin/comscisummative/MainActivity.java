e6b3bed4e0472621e2a19bb8f5384976707770c9
Michel Vezarov <mvezarov@gmail.com>
2017-11-01 12:27:23 -0400

    Initial commit, splash screen created

---
+++ app/src/main/java/com/comsci/michelaustin/comscisummative/MainActivity.java
@@ -0,0 +1,15 @@
+package com.comsci.michelaustin.comscisummative;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        setTheme(R.style.AppTheme);
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
