27ba1893be4b39ef2daaddfd9aa8fe09881069ca
Michel Vezarov <mvezarov@gmail.com>
2017-11-03 13:17:55 -0400

    First opening detection method completed, opens different layout on the first opening after app installation.

--- app/src/main/java/com/comsci/michelaustin/comscisummative/MainActivity.java
+++ app/src/main/java/com/comsci/michelaustin/comscisummative/MainActivity.java
@@ -1,15 +1,25 @@
 package com.comsci.michelaustin.comscisummative;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+    SharedPreferences prefs = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         setTheme(R.style.AppTheme);
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        prefs = getSharedPreferences("com.comsci.michelaustin.comscisummative", MODE_PRIVATE);
+
+        if (prefs.getBoolean("firstrun", true)) {
+          setContentView(R.layout.activity_firstopening);
+          prefs.edit().putBoolean("firstrun", false).commit();
+        }
+        else{
+            setContentView(R.layout.activity_main);
+        }
 
     }
 }
