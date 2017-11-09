1bd6aaf022718e308fae63129e1e91693dcc5ccd
Michel Vezarov <mvezarov@gmail.com>
2017-11-09 13:26:06 -0500

    Added user input name popup

--- app/src/main/java/com/comsci/michelaustin/comscisummative/firstopening.java
+++ app/src/main/java/com/comsci/michelaustin/comscisummative/firstopening.java
@@ -1,13 +1,20 @@
 package com.comsci.michelaustin.comscisummative;
 
+import android.content.Context;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.Toast;
 
 public class firstopening extends AppCompatActivity {
     ImageButton buoyButton, infoButton;
+    final Context context = this;
+    private EditText result;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,9 +25,44 @@ public class firstopening extends AppCompatActivity {
         infoButton = findViewById(R.id.info);
 
         buoyButton.setOnClickListener(new View.OnClickListener() {
+
             @Override
             public void onClick(View v) {
-                Toast.makeText(getBaseContext(), "lgbuoy", Toast.LENGTH_SHORT).show();
+                // get prompts.xml view
+                LayoutInflater li = LayoutInflater.from(context);
+                View promptsView = li.inflate(R.layout.promp, null);
+
+                AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(
+                        context);
+
+                // set prompts.xml to alertdialog builder
+                alertDialogBuilder.setView(promptsView);
+
+                final EditText userInput = (EditText) promptsView
+                        .findViewById(R.id.editTextDialogUserInput);
+
+                // set dialog message
+                alertDialogBuilder
+                        .setCancelable(false)
+                        .setPositiveButton("OK",
+                                new DialogInterface.OnClickListener() {
+                                    public void onClick(DialogInterface dialog,int id) {
+                                        // get user input and set it to result
+                                        // edit text
+                                    }
+                                })
+                        .setNegativeButton("Cancel",
+                                new DialogInterface.OnClickListener() {
+                                    public void onClick(DialogInterface dialog,int id) {
+                                        dialog.cancel();
+                                    }
+                                });
+
+                // create alert dialog
+                AlertDialog alertDialog = alertDialogBuilder.create();
+
+                // show it
+                alertDialog.show();
             }
         });
 
