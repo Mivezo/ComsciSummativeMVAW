cf6c61080f761be26dbb26a2d1d52eed868d0c5e
Michel Vezarov <mvezarov@gmail.com>
2017-11-20 14:08:51 -0500

    Merged Austins Code with my own, manually done.

--- app/src/main/java/com/comsci/michelaustin/comscisummative/menuopening.java
+++ app/src/main/java/com/comsci/michelaustin/comscisummative/menuopening.java
@@ -1,12 +1,16 @@
 package com.comsci.michelaustin.comscisummative;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 public class menuopening extends AppCompatActivity {
 
     String name = MainActivity.userName;
+    ImageButton menuBuoyButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +20,17 @@ public class menuopening extends AppCompatActivity {
 
         TextView nameshow = (TextView) findViewById(R.id.menuname);
         nameshow.setText(name);
+
+        menuBuoyButton = findViewById(R.id.menuBuoy);
+
+        menuBuoyButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent startQuiz = new Intent(getApplicationContext(), QuizMenuActivity.class);
+                startQuiz.putExtra("MODULE_ID",1);
+                startActivity(startQuiz);
+            }
+        });
 
     }
 }
