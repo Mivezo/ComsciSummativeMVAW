de4dbfa76b013ae92f0cca8f48ac6b85320ccec1
Michel Vezarov <mvezarov@gmail.com>
2017-11-20 14:09:28 -0500

    Merged Austins Code with my own, manually done. #2

---
+++ app/src/main/java/com/comsci/michelaustin/comscisummative/QuestionLibrary.java
@@ -0,0 +1,205 @@
+package com.comsci.michelaustin.comscisummative;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Austin on 2017-11-06.
+ */
+
+public class QuestionLibrary {
+
+    private int moduleNumber;
+
+    public QuestionLibrary(int m){
+        moduleNumber=m;
+    }
+
+    //string array of questions
+    private String questions1[] = {
+            "What is the medical meaning of shock?",
+            "Can shock be lethal?",
+            "Tap possible causes of shock",
+            "What is not a symptom of shock?",
+            "What is not an appropriate way to treat shock"
+    };
+
+    private String questions2[] = {
+            "What is the least prominent symptom of a fully obscured airway?",
+            "How big must a toy be to be considered a choking hazard for toddlers?",
+            "When performing back blows, where must the hits be directed?",
+            "Which of these should be performed first?",
+            "When a person becomes unconscious and/or unresponsive, what must you begin after calling 911?"
+    };
+    private String questions3[] = {
+            "What is the medical meaning of shock?",
+            "Can shock be lethal?",
+            "Tap possible causes of shock",
+            "What is not a symptom of shock?",
+            "What is not an appropriate way to treat shock"
+    };
+    private String questions4[] = {
+            "What is the medical meaning of shock?",
+            "Can shock be lethal?",
+            "Tap possible causes of shock",
+            "What is not a symptom of shock?",
+            "What is not an appropriate way to treat shock"
+    };
+    private String questions5[] = {
+            "What is the medical meaning of shock?",
+            "Can shock be lethal?",
+            "Tap possible causes of shock",
+            "What is not a symptom of shock?",
+            "What is not an appropriate way to treat shock"
+    };
+
+    //string array of answers for each specific question
+    private String answers1[][]={
+            {"Vital organs lack oxygen rich blood","Cut off circulation to the limbs", "Fright caused by a certain situation", "Excessive shaking"},
+            {"Yes", "No", "", ""},
+            {"Vomiting and diarrhoea", "Significant blood loss", "Severe infection", "Contusion to the brain"},
+            {"Excessive thirst", "Rapid breathing", "Pale skin", "Eyes roll to back of head"},
+            {"Warming up the victim", "Having the victim lay down in a comfortable position", "Performing cpr on the victim", "Monitor airway and breathing"}
+    };
+
+    private String answers2[][]={
+            {"Excessive arm movements","No sound coming out of the victim", "Red flushed face", "One or both hands clutching the throat"},
+            {"1 inch ", "4 cm", "2 cm", "1.5 cm"},
+            {"Base of the neck", "Between the shoulder blades", "Middle of the back", "Back of the head"},
+            {"Back Blows", "Abdominal Thrusts", "", ""},
+            {"Continue back blows and chest compressions", "Perform CPR","Attempt to the dislodge the object by pulling it out", "Nothing, wait for 911"}
+    };
+
+    //string array of correct answers
+    private String correctAnswers1[][] = {
+            { "Vital organs lack oxygen rich blood"},
+            {"Yes"},
+            {"Vomiting and diarrhoea", "Significant blood loss", "Severe infection"},
+            {"Eyes roll to back of head"},
+            {"Performing cpr on the victim"}
+    };
+
+    private String correctAnswers2[][] = {
+            {"Excessive arm movements"},
+            {"4 cm"},
+            {"Between the shoulder blades"},
+            {"Back Blows"},
+            {"Perform CPR"}
+    };
+
+
+    private String explanations1[]={
+            "Explanation1",
+            "Explanation2",
+            "Explanation3",
+            "Explanation4",
+            "Explanation5"
+    };
+
+    private String explanations2[]={
+            "Explanationa",
+            "Explanationb",
+            "Explanationc",
+            "Explanationd",
+            "Explanatione"
+    };
+
+    private int questionAmount = 5;
+
+    ArrayList currentCorrectAnswerArray = new ArrayList();
+
+    //Returns the specific question in the array
+    public String getQuestion(int i){
+        switch (moduleNumber) {
+            case 1:
+                return questions1[i];
+            case 2:
+                return questions2[i];
+            case 3:
+                return questions3[i];
+            case 4:
+                return questions4[i];
+            case 5:
+                return questions5[i];
+            default:return "0";
+
+        }
+
+    }
+
+    public String getChoice(int i,int q){
+
+        switch (moduleNumber){
+            case 1:
+                return isAnswerBlank(answers1, i, q);
+            case 2:
+                return isAnswerBlank(answers2, i, q);
+            default: return "0";
+
+        }
+
+    }
+
+    public String getExplanation(int i){
+
+        switch (moduleNumber){
+            case 1:
+                return explanations1[i];
+            case 2:
+                return explanations2[i];
+            default: return "0";
+        }
+
+    }
+
+
+    //returns the correct answer based on the question
+    public ArrayList getCorrectAnswer(int i){
+
+        currentCorrectAnswerArray.clear();
+
+        switch (moduleNumber){
+            case 1:
+                addCorrectAnswerToArray(correctAnswers1, i);
+            case 2:
+                addCorrectAnswerToArray(correctAnswers2, i);
+        }
+
+        return currentCorrectAnswerArray;
+    }
+
+    //for getCorrectAnswer method, adds correct answers to arraylist
+    private void addCorrectAnswerToArray(String[][] array, int i){
+        for(int j = 0; j<array[i].length; j++){
+            currentCorrectAnswerArray.add(array[i][j]);
+        }
+    }
+
+    //for the getchoice method, returns a string
+    public String isAnswerBlank(String[][] array, int i, int q){
+        if(array[i][q]!=""){
+            String choice= array[i][q];
+            return choice;
+        }
+        else{
+            return "";
+        }
+    }
+
+
+    //Returns the number of possible correct answers
+    public int getNumCorrect(int c){
+
+        switch (moduleNumber){
+            case 1: return correctAnswers1[c].length;
+            case 2: return correctAnswers2[c].length;
+            default: return 0;
+        }
+
+    }
+
+    //returns the amount of questions
+    public int getQuestionAmount(){
+        return questionAmount;
+    }
+
+}
---
+++ app/src/main/java/com/comsci/michelaustin/comscisummative/QuizMenuActivity.java
@@ -0,0 +1,230 @@
+package com.comsci.michelaustin.comscisummative;
+
+import android.app.Dialog;
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+public class QuizMenuActivity extends AppCompatActivity {
+
+    private TextView questionLabel;
+    private Button option1;
+    private Button option2;
+    private Button option3;
+    private Button option4;
+    /*private String mAnswer;*/
+    private int questionNumber=0;
+    private int amountCorrect;//integer needed if there are multiple answers
+    private int amountCorrectComparison=0;//integer to compare
+    private String explanation="";
+
+    private int moduleNumber;
+
+    private Dialog dialog;//dialog for popup
+
+
+    ArrayList answerArray = new ArrayList();
+
+
+    private QuestionLibrary mQuestionLibrary;//QuestionLibrary Object
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_quiz_menu);
+        moduleNumber = getIntent().getIntExtra("MODULE_ID", 0);
+
+        //initializing the buttons as well as the question labels
+        option1 = (Button) findViewById(R.id.option1);
+        option2 = (Button) findViewById(R.id.option2);
+        option3 = (Button) findViewById(R.id.option3);
+        option4 = (Button) findViewById(R.id.option4);
+        questionLabel = (TextView) findViewById(R.id.questionLabel);
+
+        mQuestionLibrary = new QuestionLibrary(moduleNumber);
+
+        displayQuestions(); //displaying the questions on the option buttons as well as retrieving specific question info
+
+        //creating a dialog for the popup window
+        dialog = new Dialog(this);
+
+        // Sets onclick listener for each button to test whether the user clicked the right one
+        option1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                checkAnswer(option1);
+            }
+        });
+        option2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                checkAnswer(option2);
+            }
+        });
+        option3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                checkAnswer(option3);
+            }
+        });
+        option4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                checkAnswer(option4);
+            }
+        });
+
+    }
+
+    //Displays the questions on the screen as well as fetches the correct answer
+    //Also fetches the correct amount of answers to test to allow for multiple answers
+    private void displayQuestions(){
+        questionLabel.setText(mQuestionLibrary.getQuestion(questionNumber));
+
+        option1.setText(getQuestion(questionNumber,0));
+        option1.setBackgroundColor(Color.WHITE);
+        option2.setText(getQuestion(questionNumber,1));
+        option2.setBackgroundColor(Color.WHITE);
+        option3.setBackgroundColor(Color.WHITE);
+        option4.setBackgroundColor(Color.WHITE);
+        //to test specifically for true and false questions and to remove visibility of the last two buttons
+        if (testWhetherBlank(2)) {
+            option3.setVisibility(View.GONE);
+        }
+        else{
+            option3.setVisibility(View.VISIBLE);
+            option3.setText(getQuestion(questionNumber,2));
+        }
+
+        if (testWhetherBlank(3)) {
+            option4.setVisibility(View.GONE);
+        }
+        else{
+            option4.setVisibility(View.VISIBLE);
+            option4.setText(getQuestion(questionNumber,3));
+        }
+
+        //
+        answerArray=(ArrayList<Object>)mQuestionLibrary.getCorrectAnswer(questionNumber).clone();
+
+        explanation=mQuestionLibrary.getExplanation(questionNumber);
+
+        amountCorrect=mQuestionLibrary.getNumCorrect(questionNumber);
+
+
+    }
+
+    //allows the UI to test and remove buttons if the options are blank
+    public boolean testWhetherBlank(int q){
+        if(mQuestionLibrary.getChoice(questionNumber,q).equals("")){
+            return true;
+        }
+        else{
+            return false;
+        }
+
+    }
+
+
+    //fetches the correct question from the QuestionLibrary class
+    private String getQuestion(int q, int n){
+        String result = mQuestionLibrary.getChoice(q,n);
+        return result;
+    }
+
+    //Testing whether the user chose all possible answers
+    private boolean testComplete(){
+        if(amountCorrectComparison == amountCorrect){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+
+    private boolean testFullyComplete(){
+        if(questionNumber == mQuestionLibrary.getQuestionAmount()){
+            return true;
+        }
+        else return false;
+    }
+
+    //checks whether the button pressed is a correct answer or not
+    private void checkAnswer(Button b){
+
+        boolean correct = false;
+
+        for (int i = 0; i<answerArray.size(); i++){
+            //compares the text on the button with the arraylist
+            if(b.getText()== answerArray.get(i)){
+                b.setBackgroundColor(Color.GREEN); //sets to green to indicate correct answer
+                amountCorrectComparison++; //increment integer to compare with set number of correct answers
+                correct=true;
+
+                if(testComplete()){
+                    questionNumber+=1;
+                    showPopup(this.findViewById(android.R.id.content));
+                }
+            }
+        }
+        if(!correct){
+            b.setBackgroundColor(Color.RED);
+        }
+
+    }
+
+
+
+    //Adding delay to the button to make green visible
+    private void switchQuestion(){
+        final Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                // Do something after 1.5s = 1500ms
+                if(!testFullyComplete()){
+                    displayQuestions();
+                }
+                else displayCompletionScreen();
+            }
+        }, 1000);
+        amountCorrectComparison=0;
+    }
+
+    //shows the explanation popup
+    public void showPopup(View v) {
+        TextView nextButton;
+        TextView explanationLabel;
+        TextView explanationText;
+        dialog.setContentView(R.layout.custompopup);
+
+        explanationLabel = (TextView) dialog.findViewById(R.id.explanationLabel);
+        nextButton = (TextView) dialog.findViewById(R.id.nextText);
+        explanationText = (TextView) dialog.findViewById(R.id.explanationText);
+        explanationText.setText(explanation);
+
+        //Switches the question when the next button is pressed
+        nextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dialog.dismiss();
+                switchQuestion();
+            }
+        });
+        dialog.show();
+    }
+
+    public void displayCompletionScreen(){
+        Intent startIntent= new Intent(getApplicationContext(), QuizCompletionActivity.class);
+        startActivity(startIntent);
+    }
+}
