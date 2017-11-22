99870f6c1e507ad7d0d679db60643cf20167c20c
Austin Wu <austinwu28@gmail.com>
2017-11-22 14:46:09 -0500

    Added menu button to completion

--- app/src/main/java/com/comsci/michelaustin/comscisummative/QuizCompletionActivity.java
+++ app/src/main/java/com/comsci/michelaustin/comscisummative/QuizCompletionActivity.java
@@ -1,13 +1,29 @@
 package com.comsci.michelaustin.comscisummative;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class QuizCompletionActivity extends AppCompatActivity {
+
+    private Button menuButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz_completion);
+
+        menuButton = (Button) findViewById(R.id.menuButton);
+        menuButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent startIntent = new Intent(getApplication(), menuopening.class);
+                startActivity(startIntent);
+            }
+        });
+
+
     }
 }
