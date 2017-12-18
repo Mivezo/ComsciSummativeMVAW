4dd56f4612d28d9f1f11289de0b6cc8fde82f3e1
Austin Wu <austinwu28@gmail.com>
2017-12-18 13:20:36 -0500

    Added inclusion of testModule to the QuestionLibrary class

--- app/src/main/java/com/comsci/michelaustin/comscisummative/QuestionLibrary.java
+++ app/src/main/java/com/comsci/michelaustin/comscisummative/QuestionLibrary.java
@@ -9,6 +9,7 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 /**
@@ -24,12 +25,18 @@ public class QuestionLibrary {
     String explanationsFileName;
     String correctAnswersFileName;
 
+    ArrayList<Integer> randomInt = new ArrayList<Integer>();
+
 
 
     private ArrayList <String> questions= new ArrayList<String>();
     private List<List<String>> answers = new ArrayList<List<String>>();
     ArrayList<ArrayList<String>> correctAnswers = new ArrayList<ArrayList<String>>();
     private ArrayList <String> explanations= new ArrayList<String>();
+
+    private ArrayList <String> testQuestions= new ArrayList<String>();
+    private List<List<String>> testAnswers = new ArrayList<List<String>>();
+    ArrayList<ArrayList<String>> testCorrectAnswers = new ArrayList<ArrayList<String>>();
 
 
 
@@ -40,30 +47,41 @@ public class QuestionLibrary {
         moduleID=mID;
         context=c;
 
-        questionFileName="questions"+moduleID+".txt";
-        answerFileName="answers"+moduleID+".txt";
-        explanationsFileName="explanations"+moduleID+".txt";
-        correctAnswersFileName="correctanswers"+moduleID+".txt";
+        createFileNames();
 
 
         buildArrays();
+        if(moduleID==6) randomizeArrays();
+
+    }
+
+    private void createFileNames(){
+        if(moduleID!=6){
+            explanationsFileName="explanations"+moduleID+".txt";
+        }
+        questionFileName="questions"+moduleID+".txt";
+        answerFileName="answers"+moduleID+".txt";
+        correctAnswersFileName="correctanswers"+moduleID+".txt";
     }
 
     /**
      * Creates all arrays
      */
     private void buildArrays(){
+        //adds arbitrary values to the arraylist as using the arrays.clear method returns null if there are no preassigned values
         questions.add("!");
         answers.add(Arrays.asList("1"));
-        explanations.add("!");
         correctAnswers.add(new ArrayList<String>());
         correctAnswers.get(0).add("1");
 
-        //Log.d("Test", answerFileName);
+        //no explanations needed for the test
+       if(moduleID!=6){
+           explanations.add("!");
+           createArray(explanations, explanationsFileName);
+       }
 
         createArray(questions, questionFileName);
         createAnswerArray(answerFileName);
-        createArray(explanations, explanationsFileName);
         createCorrectAnswerArray(correctAnswersFileName);
     }
 
@@ -78,6 +96,10 @@ public class QuestionLibrary {
 
     }
 
+    /**
+     * This method creates the answer arraylist based off of the answer txt file
+     * @param fn filename
+     */
     private void createAnswerArray(String fn){
 
         answers.clear();
@@ -113,6 +135,10 @@ public class QuestionLibrary {
 
     }
 
+    /**
+     * Creates the correct answer arraylist based off of the txt file
+     * @param fn
+     */
     private void createCorrectAnswerArray(String fn){
 
         correctAnswers.clear();
@@ -174,48 +200,89 @@ public class QuestionLibrary {
 
     //returns the answer choices for the buttons
     public String getChoice(int i,int q){
-        return isAnswerBlank(i,q);
+        if(moduleID!=6){
+            return isAnswerBlank(i,q,answers);
+        }
+        else{
+            return isAnswerBlank(i,q,testAnswers); //for test module
+        }
+
     }
 
     public String getExplanation(int i){
-        return explanations.get(i);
+        if(moduleID!=6){
+            return explanations.get(i);
+        }
+        else return null;//for test module
     }
 
     public String getQuestion(int i){
-        return questions.get(i);
+        if(moduleID!=6){
+            return questions.get(i);
+        }
+        else{
+            return testQuestions.get(i);//for test module
+        }
     }
 
     public ArrayList getCorrectAnswer(int i){
-        return correctAnswers.get(i);
+        if(moduleID!=6){
+            return correctAnswers.get(i);
+        }
+        else return testCorrectAnswers.get(i);//for test module
+
     }
 
     //returns the amount of questions
     public int getAnswerAmount(int i){
-        answerAmount=correctAnswers.get(i).size();
+        if(moduleID!=6){
+            answerAmount=correctAnswers.get(i).size();
+        }
+        else answerAmount=testCorrectAnswers.get(i).size();//for test module
+
         return answerAmount;
     }
 
+    //Shuffles the question, answer and correctanswer arrays. Takes the 1st 20 and puts it into a new arraylist with the prefixes test
+    public void randomizeArrays(){
+        for(int i=0; i<randomInt.size(); i++){
+            randomInt.add(i);
+        }
+        Collections.shuffle(Arrays.asList(randomInt));
+
+        for(int i=0; i<20; i++){
+            testQuestions.add(questions.get(randomInt.get(i)));
+            testAnswers.add(answers.get(randomInt.get(i)));
+            testCorrectAnswers.add(correctAnswers.get(randomInt.get(i)));
+        }
+
+
+    }
+
     public int getQuestionAmount(){
-        return questions.size();
+        if(moduleID!=6){
+            return questions.size();
+        }
+        else return testQuestions.size();
+
     }
 
     /**
      * Tests whether the answer is blank or not to delete the button
      * @param i int row
      * @param q int column
+     * @param a 2d answer arraylist
      * @return
      */
-    public String isAnswerBlank(int i, int q){
-        if(!answers.get(i).get(q).equals(" ")){
-            String choice= answers.get(i).get(q);
+    public String isAnswerBlank(int i, int q, List<List<String>> a){
+        if(!a.get(i).get(q).equals(" ")){
+            String choice= a.get(i).get(q);
             return choice;
         }
         else{
             return "";
         }
     }
-
-
 
 
     /**
@@ -242,7 +309,6 @@ public class QuestionLibrary {
             ioe.printStackTrace();
         }
     }
-
 
 
     private void test(){
