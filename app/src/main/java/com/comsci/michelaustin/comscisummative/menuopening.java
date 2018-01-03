77ca8f3ba09f69c1fa3b7a53f012bf16f88b8a7e
Austin Wu <austinwu28@gmail.com>
2018-01-03 16:23:42 -0500

    Added button animation for menu

--- app/src/main/java/com/comsci/michelaustin/comscisummative/menuopening.java
+++ app/src/main/java/com/comsci/michelaustin/comscisummative/menuopening.java
@@ -1,10 +1,15 @@
 package com.comsci.michelaustin.comscisummative;
 
+import android.animation.AnimatorSet;
+import android.animation.ObjectAnimator;
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
+import android.util.DisplayMetrics;
 import android.view.View;
+import android.view.animation.AlphaAnimation;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.ImageButton;
@@ -14,6 +19,9 @@ public class menuopening extends AppCompatActivity {
 
     String name = MainActivity.userName;
     ImageButton module1Button, module2Button, module3Button, module4Button, module5Button, module6Button;
+    int height, width;
+
+    DisplayMetrics displayMetrics = new DisplayMetrics();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,6 +37,12 @@ public class menuopening extends AppCompatActivity {
         TextView welcome = (TextView) findViewById(R.id.welcome);
         welcome.setText("Please pick a\nModule:");
 
+        getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
+        height = displayMetrics.heightPixels/2;
+        width = displayMetrics.widthPixels/2;
+
+
+
         (module1Button = findViewById(R.id.module1)).startAnimation(shake);
         (module2Button = findViewById(R.id.module2)).startAnimation(shake);
         (module3Button = findViewById(R.id.module3)).startAnimation(shake);
@@ -39,13 +53,25 @@ public class menuopening extends AppCompatActivity {
         module1Button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
+                animateButton(module1Button);
+
                 module2Button.setEnabled(false);
                 module3Button.setEnabled(false);
                 module4Button.setEnabled(false);
                 module5Button.setEnabled(false);
-                Intent startQuiz = new Intent(getApplicationContext(), QuizMenuActivity.class);
-                startQuiz.putExtra("MODULE_ID",1);
-                startActivity(startQuiz);
+
+                final Handler handler = new Handler();
+                handler.postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        // Do something after a certain amount of time
+                        Intent startQuiz = new Intent(getApplicationContext(), QuizMenuActivity.class);
+                        startQuiz.putExtra("MODULE_ID",1);
+                        startActivity(startQuiz);
+                    }
+                }, 2000);
+
             }
         });
 
@@ -88,6 +114,102 @@ public class menuopening extends AppCompatActivity {
                 startActivity(startQuiz);
             }
         });
+    }
+
+    public void animateButton(ImageButton b) {
+        b.clearAnimation();
+
+        ObjectAnimator animation1 = ObjectAnimator.ofFloat(b, "translationY", 0, height - b.getHeight()/2-b.getY());
+        animation1.setDuration(1000);
+
+        ObjectAnimator animation2 = ObjectAnimator.ofFloat(b, "translationX", 0, width - b.getWidth()/2-b.getX());
+        animation2.setDuration(1000);
+
+        ObjectAnimator animation3 = ObjectAnimator.ofFloat(b, "scaleX", 0, 5);
+        animation3.setDuration(1000);
+
+        ObjectAnimator animation4 = ObjectAnimator.ofFloat(b, "scaleY", 0, 5);
+        animation4.setDuration(1000);
+
+        AnimatorSet animatorSet = new AnimatorSet();
+        animatorSet.playTogether(animation1, animation2,animation3, animation4);
+        animatorSet.start();
+
+        int buttonId = b.getId();
+
+        switch(buttonId){
+            case R.id.module1:
+                module3Button.setEnabled(false);
+                module2Button.setEnabled(false);
+                module4Button.setEnabled(false);
+                module5Button.setEnabled(false);
+                module6Button.setEnabled(false);
+                fadeButton(module2Button);
+                fadeButton(module3Button);
+                fadeButton(module4Button);
+                fadeButton(module5Button);
+                fadeButton(module6Button);
+                break;
+            case R.id.module2:
+                module1Button.setEnabled(false);
+                module3Button.setEnabled(false);
+                module4Button.setEnabled(false);
+                module5Button.setEnabled(false);
+                module6Button.setEnabled(false);
+                fadeButton(module1Button);
+                fadeButton(module3Button);
+                fadeButton(module4Button);
+                fadeButton(module5Button);
+                fadeButton(module6Button);
+                break;
+            case R.id.module3:
+                module1Button.setEnabled(false);
+                module2Button.setEnabled(false);
+                module4Button.setEnabled(false);
+                module5Button.setEnabled(false);
+                module6Button.setEnabled(false);
+                fadeButton(module2Button);
+                fadeButton(module1Button);
+                fadeButton(module4Button);
+                fadeButton(module5Button);
+                fadeButton(module6Button);
+                break;
+            case R.id.module4:
+                module1Button.setEnabled(false);
+                module2Button.setEnabled(false);
+                module3Button.setEnabled(false);
+                module5Button.setEnabled(false);
+                module6Button.setEnabled(false);
+                fadeButton(module2Button);
+                fadeButton(module3Button);
+                fadeButton(module1Button);
+                fadeButton(module5Button);
+                fadeButton(module6Button);
+                break;
+            case R.id.module5:
+                module1Button.setEnabled(false);
+                module2Button.setEnabled(false);
+                module4Button.setEnabled(false);
+                module3Button.setEnabled(false);
+                module6Button.setEnabled(false);
+                fadeButton(module2Button);
+                fadeButton(module3Button);
+                fadeButton(module4Button);
+                fadeButton(module1Button);
+                fadeButton(module6Button);
+                break;
+            default:
+
+
+        }
+    }
+
+    public void fadeButton(ImageButton b){
+        Animation out = new AlphaAnimation(1.0f, 0.0f);
+        out.setDuration(1000);
+
+        b.startAnimation(out);
+        b.setVisibility(View.INVISIBLE);
     }
 
     @Override
