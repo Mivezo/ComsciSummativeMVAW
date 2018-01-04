0b943ba4a792ced313701e218f95e6baf0305335
Austin Wu <austinwu28@gmail.com>
2018-01-04 11:20:09 -0500

    Tweaking dialog popup

--- app/src/main/java/com/comsci/michelaustin/comscisummative/menuopening.java
+++ app/src/main/java/com/comsci/michelaustin/comscisummative/menuopening.java
@@ -2,10 +2,10 @@ package com.comsci.michelaustin.comscisummative;
 
 import android.animation.AnimatorSet;
 import android.animation.ObjectAnimator;
+import android.app.Dialog;
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
-import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.util.DisplayMetrics;
 import android.view.View;
@@ -14,12 +14,17 @@ import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.ImageButton;
 import android.widget.TextView;
+import android.os.Handler;
 
 public class menuopening extends AppCompatActivity {
 
     String name = MainActivity.userName;
     ImageButton module1Button, module2Button, module3Button, module4Button, module5Button, module6Button;
     int height, width;
+
+    TextView nameshow, welcome, mod1, mod2, mod3, mod4, mod5;
+
+    private Dialog dialog;
 
     DisplayMetrics displayMetrics = new DisplayMetrics();
 
@@ -31,17 +36,23 @@ public class menuopening extends AppCompatActivity {
         fileIo.writeFile(name,"lifeguardname.txt",this);
         Animation shake = AnimationUtils.loadAnimation(this, R.anim.turn);
 
-        TextView nameshow = (TextView) findViewById(R.id.menuname);
+        dialog = new Dialog(this,android.R.style.Theme_Black_NoTitleBar_Fullscreen);
+
+        nameshow = (TextView) findViewById(R.id.menuname);
         nameshow.setText("Welcome " + name+ "!");
 
-        TextView welcome = (TextView) findViewById(R.id.welcome);
+        welcome = (TextView) findViewById(R.id.welcome);
         welcome.setText("Please pick a\nModule:");
 
         getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
         height = displayMetrics.heightPixels/2;
         width = displayMetrics.widthPixels/2;
 
-
+        mod1 = findViewById(R.id.mod1);
+        mod2 = findViewById(R.id.mod2);
+        mod3 = findViewById(R.id.mod3);
+        mod4 = findViewById(R.id.mod4);
+        mod5 = findViewById(R.id.mod5);
 
         (module1Button = findViewById(R.id.module1)).startAnimation(shake);
         (module2Button = findViewById(R.id.module2)).startAnimation(shake);
@@ -54,23 +65,9 @@ public class menuopening extends AppCompatActivity {
             @Override
             public void onClick(View v) {
 
-                animateButton(module1Button);
+                animateButton(module1Button, mod1);
 
-                module2Button.setEnabled(false);
-                module3Button.setEnabled(false);
-                module4Button.setEnabled(false);
-                module5Button.setEnabled(false);
-
-                final Handler handler = new Handler();
-                handler.postDelayed(new Runnable() {
-                    @Override
-                    public void run() {
-                        // Do something after a certain amount of time
-                        Intent startQuiz = new Intent(getApplicationContext(), QuizMenuActivity.class);
-                        startQuiz.putExtra("MODULE_ID",1);
-                        startActivity(startQuiz);
-                    }
-                }, 2000);
+          /*      */
 
             }
         });
@@ -114,9 +111,11 @@ public class menuopening extends AppCompatActivity {
                 startActivity(startQuiz);
             }
         });
+
+
     }
 
-    public void animateButton(ImageButton b) {
+    public void animateButton(ImageButton b, TextView t) {
         b.clearAnimation();
 
         ObjectAnimator animation1 = ObjectAnimator.ofFloat(b, "translationY", 0, height - b.getHeight()/2-b.getY());
@@ -125,17 +124,34 @@ public class menuopening extends AppCompatActivity {
         ObjectAnimator animation2 = ObjectAnimator.ofFloat(b, "translationX", 0, width - b.getWidth()/2-b.getX());
         animation2.setDuration(1000);
 
-        ObjectAnimator animation3 = ObjectAnimator.ofFloat(b, "scaleX", 0, 5);
+        ObjectAnimator animation3 = ObjectAnimator.ofFloat(b, "scaleX", 1, 5);
         animation3.setDuration(1000);
 
-        ObjectAnimator animation4 = ObjectAnimator.ofFloat(b, "scaleY", 0, 5);
+        ObjectAnimator animation4 = ObjectAnimator.ofFloat(b, "scaleY", 1, 5);
         animation4.setDuration(1000);
 
+        ObjectAnimator animation5 = ObjectAnimator.ofFloat(t, "translationY", 0, height - t.getHeight()/2-t.getY());
+        animation5.setDuration(1000);
+
+        ObjectAnimator animation6 = ObjectAnimator.ofFloat(t, "translationX", 0, width - t.getWidth()/2-t.getX());
+        animation6.setDuration(1000);
+
+        ObjectAnimator animation7 = ObjectAnimator.ofFloat(t, "scaleX", 1, 4);
+        animation7.setDuration(1000);
+
+        ObjectAnimator animation8 = ObjectAnimator.ofFloat(t, "scaleY", 1, 4);
+        animation8.setDuration(1000);
+
+
         AnimatorSet animatorSet = new AnimatorSet();
-        animatorSet.playTogether(animation1, animation2,animation3, animation4);
+        animatorSet.playTogether(animation1, animation2,animation3, animation4, animation5, animation6, animation7, animation8);
         animatorSet.start();
 
         int buttonId = b.getId();
+
+        fadeText(nameshow);
+        fadeText(welcome);
+
 
         switch(buttonId){
             case R.id.module1:
@@ -149,6 +165,10 @@ public class menuopening extends AppCompatActivity {
                 fadeButton(module4Button);
                 fadeButton(module5Button);
                 fadeButton(module6Button);
+                fadeText(mod2);
+                fadeText(mod3);
+                fadeText(mod4);
+                fadeText(mod5);
                 break;
             case R.id.module2:
                 module1Button.setEnabled(false);
@@ -161,6 +181,10 @@ public class menuopening extends AppCompatActivity {
                 fadeButton(module4Button);
                 fadeButton(module5Button);
                 fadeButton(module6Button);
+                fadeText(mod1);
+                fadeText(mod3);
+                fadeText(mod4);
+                fadeText(mod5);
                 break;
             case R.id.module3:
                 module1Button.setEnabled(false);
@@ -173,6 +197,10 @@ public class menuopening extends AppCompatActivity {
                 fadeButton(module4Button);
                 fadeButton(module5Button);
                 fadeButton(module6Button);
+                fadeText(mod2);
+                fadeText(mod1);
+                fadeText(mod4);
+                fadeText(mod5);
                 break;
             case R.id.module4:
                 module1Button.setEnabled(false);
@@ -185,6 +213,10 @@ public class menuopening extends AppCompatActivity {
                 fadeButton(module1Button);
                 fadeButton(module5Button);
                 fadeButton(module6Button);
+                fadeText(mod2);
+                fadeText(mod3);
+                fadeText(mod1);
+                fadeText(mod5);
                 break;
             case R.id.module5:
                 module1Button.setEnabled(false);
@@ -197,19 +229,55 @@ public class menuopening extends AppCompatActivity {
                 fadeButton(module4Button);
                 fadeButton(module1Button);
                 fadeButton(module6Button);
+                fadeText(mod2);
+                fadeText(mod3);
+                fadeText(mod4);
+                fadeText(mod1);
                 break;
-            default:
-
-
         }
+
+        final Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                // Do something after a certain amount of time
+                showMenuPopup();
+            }
+        }, 1000);
+
     }
 
     public void fadeButton(ImageButton b){
         Animation out = new AlphaAnimation(1.0f, 0.0f);
-        out.setDuration(1000);
+        out.setDuration(500);
 
         b.startAnimation(out);
         b.setVisibility(View.INVISIBLE);
+    }
+
+    public void fadeText(TextView t){
+        Animation out = new AlphaAnimation(1.0f, 0.0f);
+        out.setDuration(500);
+
+        t.startAnimation(out);
+        t.setVisibility(View.INVISIBLE);
+    }
+
+    public void showMenuPopup(){
+        dialog.setContentView(R.layout.menu_popup);
+
+        ImageButton play  =  dialog.findViewById(R.id.nextButton);
+
+        play.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dialog.dismiss();
+                Intent startQuiz = new Intent(getApplicationContext(), QuizMenuActivity.class);
+                startQuiz.putExtra("MODULE_ID",1);
+                startActivity(startQuiz);
+            }
+        });
+        dialog.show();
     }
 
     @Override
